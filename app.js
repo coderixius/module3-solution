5b21ab0e82c75d1e459cc0a68d1a70408702f101
sakis papadopoulos <you@example.com>
2021-01-17 00:20:27 +0200

    New solution

--- app.js
+++ app.js
@@ -1,86 +1,80 @@
-(function(){
-'use strict';
+(function() {
+  'use strict';
 
+  angular.module('NarrowItDownApp', [])
+    .controller('NarrowItDownController', NarrowItDownController)
+    .service('MenuSearchService', MenuSearchService)
+    .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com/menu_items.json")
+    .directive('foundItems', FoundItemsDirective);
 
-angular.module('NarrowItDownApp',[])
-.controller('NarrowItDownController', NarrowItDownController)
-.service('MenuSearchService', MenuSearchService)
-.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com/menu_items.json")
-.directive('foundItems', FoundItemsDirective);
+  function FoundItemsDirective() {
+      var ddo = {
+          templateUrl: 'foundItems.html',
+          scope: {
+              items: '<',
+              onRemove: '&'
+          },
+          controller: FoundItemsDirectiveController,
+          controllerAs: 'ctrl',
+          bindToController: true
+      };
 
-function FoundItemsDirective() {
-    var ddo = {
-        templateUrl: 'foundItems.html',
-        scope: {
-            items: '<',
-            onRemove: '&'
-        },
-        controller: FoundItemsDirectiveController,
-        controllerAs: 'ctrl',
-        bindToController: true
-    };
+      return ddo;
+  }
 
-    return ddo;
-}
+  function FoundItemsDirectiveController() {
+      var ctrl = this;
 
-function FoundItemsDirectiveController() {
-    var ctrl = this;
+      ctrl.IsEmpty = function() {
+        if (ctrl.items != undefined && ctrl.items.length === 0) {
+          return true;
+        }
 
-    ctrl.IsEmpty = function() {
-      if (ctrl.items != undefined && ctrl.items.length === 0) {
-        return true;
-      }
+        return false;
+      };
+  }
 
-      return false;
-    };
-}
+  NarrowItDownController.$inject = ['MenuSearchService'];
+  function NarrowItDownController(MenuSearchService) {
+      var ctrl = this;
 
+      ctrl.SearchTerm = "";
+     // ctrl.FoundItems = [];
 
+      ctrl.applySearch = function() {
+          var promise = MenuSearchService.getMatchedMenuItems(ctrl.SearchTerm);
 
-NarrowItDownController.$inject = ['MenuSearchService'];
-function NarrowItDownController(MenuSearchService) {
-    var ctrl = this;
+          promise.then(function(result) {
+              ctrl.FoundItems = result;
+          });
+      };
 
-    ctrl.SearchTerm = "";
-   // ctrl.FoundItems = [];
+      ctrl.remove = function (index) {
+          ctrl.FoundItems.splice(index, 1)[0];
+      };
+  }
 
-    ctrl.applySearch = function() {
-        var promise = MenuSearchService.getMatchedMenuItems(ctrl.SearchTerm);
+  MenuSearchService.$inject = ['$http', 'ApiBasePath'];
+  function MenuSearchService($http, ApiBasePath) {
+      var service = this;
 
-        promise.then(function(result) {
-            ctrl.FoundItems = result;
-        });
-    };
+      service.getMatchedMenuItems = function (searchTerm) {
+          return $http({
+              method: "GET",
+              url: ApiBasePath
+          }).then(function (result) {
+              var returnedItems = result.data.menu_items;
+              var foundItems = [];
 
-    ctrl.remove = function (index) {
-        ctrl.FoundItems.splice(index, 1)[0];
-    };
-}
+              for (var i = 0; i < returnedItems.length; i++) {
+                  if (searchTerm != "" && returnedItems[i].description.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1) {
+                      foundItems.push(returnedItems[i]);
+                  }
+              }
 
-
-
-MenuSearchService.$inject = ['$http', 'ApiBasePath'];
-function MenuSearchService($http, ApiBasePath) {
-    var service = this;
-
-    service.getMatchedMenuItems = function (searchTerm) {
-        return $http({
-            method: "GET",
-            url: ApiBasePath
-        }).then(function (result) {
-            var returnedItems = result.data.menu_items;
-            var foundItems = [];
-
-            for (var i = 0; i < returnedItems.length; i++) {
-                if (searchTerm != "" && returnedItems[i].description.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1) {
-                    foundItems.push(returnedItems[i]);
-                }
-            }
-
-            return foundItems;
-        });
-    };
-}
-
+              return foundItems;
+          });
+      };
+  }
 
 })();
